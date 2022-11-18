d9196f3212d8c1556720f2cf7948907beaa2de2d
Alexandr Matveev <kareroyal@gmail.com>
2022-11-18 18:44:43 +0300

    add env

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCljm6SNGK9IPSw6TemZJVcEyBSO9AXxZw",
-  authDomain: "test-womanup.firebaseapp.com",
-  databaseURL: "https://test-womanup-default-rtdb.firebaseio.com",
-  projectId: "test-womanup",
-  storageBucket: "test-womanup.appspot.com",
-  messagingSenderId: "858640839112",
-  appId: "1:858640839112:web:06dffa505040cb20c1d0fd",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
